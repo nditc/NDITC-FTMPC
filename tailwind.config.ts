27dc16a1a9fea897b3e34aeeae2409c9ce84b410
RafsanAmin <rafsanamin2020@gmail.com>
2024-04-04 23:06:12 +0600

    dsds

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import plugin from 'tailwindcss/plugin';
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/Components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
