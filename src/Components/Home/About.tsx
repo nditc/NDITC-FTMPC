8c488c5b7d00b568f85a6b05a7e9996cf8f11b05
The-XENO-Studios <thexenostudios@gmail.com>
2024-04-08 12:12:35 +0600

    ID for Navbar Fix

--- src/Components/Home/About.tsx
+++ src/Components/Home/About.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import Image from 'next/image';
+import Image from "next/image";
 
 const About = () => {
   return (
-    <section className="w-screen bg-no-repeat bg-cover bg-bottom  relative my-auto" id="about">
+    <section
+      className="w-screen bg-no-repeat bg-cover bg-bottom  relative my-auto"
+      id="about"
+    >
       <section className="w-screen bg-image bg-no-repeat bg-cover bg-bottom  relative my-auto">
-        {' '}
+        {" "}
         <section className="flex flex-col container items-center pt-14 pb-12 my-auto">
           <h1 className="text-[2.55rem] md:text-5xl text-white ">
             ABOUT <span className="text-secondary">FTMPC 4.0</span>
@@ -21,23 +23,29 @@ const About = () => {
             </div>
             <div className="flex-1 pb-5 mt-3">
               <p className="flex-1">
-                Embarking on the journey of knowledge and innovation, Notre Dame Information
-                Technology Club proudly presents the fourth installment of the prestigious Father
-                Timm Memorial Programming Contest (FTMPC 4.0). Named in honour of the illustrious
-                Father Richard William Timm, a luminary figure whose legacy spans the realms of
-                academia, humanitarianism, and biodiversity exploration, this programming contest
-                stands as a testament to his enduring influence. <br></br>
-                <br></br> FTMPC 4.0 is not merely a competition but a celebration of intellect and
-                creativity, aimed at nurturing the next generation of tech-savvy minds. With a nod
-                to the timeless wisdom of Steve Jobs, we believe that coding transcends the
-                boundaries of programming languages; it instills in individuals a profound ability
-                to think critically, problem-solve, and innovate.
+                Embarking on the journey of knowledge and innovation, Notre Dame
+                Information Technology Club proudly presents the fourth
+                installment of the prestigious Father Timm Memorial Programming
+                Contest (FTMPC 4.0). Named in honour of the illustrious Father
+                Richard William Timm, a luminary figure whose legacy spans the
+                realms of academia, humanitarianism, and biodiversity
+                exploration, this programming contest stands as a testament to
+                his enduring influence. <br></br>
+                <br></br> FTMPC 4.0 is not merely a competition but a
+                celebration of intellect and creativity, aimed at nurturing the
+                next generation of tech-savvy minds. With a nod to the timeless
+                wisdom of Steve Jobs, we believe that coding transcends the
+                boundaries of programming languages; it instills in individuals
+                a profound ability to think critically, problem-solve, and
+                innovate.
                 <br />
-                <br /> Open to students from diverse educational backgrounds, including schools,
-                colleges, and polytechnic institutes, this contest promises to be a transformative
-                experience. Participants will engage in the art of coding individually, employing
-                languages such as Python, C, or C++ to tackle challenges inspired by the prestigious
-                International Olympiad in Informatics (IOI) format.
+                <br /> Open to students from diverse educational backgrounds,
+                including schools, colleges, and polytechnic institutes, this
+                contest promises to be a transformative experience. Participants
+                will engage in the art of coding individually, employing
+                languages such as Python, C, or C++ to tackle challenges
+                inspired by the prestigious International Olympiad in
+                Informatics (IOI) format.
               </p>
             </div>
           </div>
